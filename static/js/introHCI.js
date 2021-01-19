aabf318d6620f68fc16fd02987a362da04d5ac1e
Brian Leung <bleung@ucsd.edu>
2021-01-19 14:39:09 -0800

    Lab 3 Initial With Extra Button for p.active Toggle

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,32 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("Ask your own heart.");
+		$(".jumbotron p").addClass("active");
 	});
 
+	$("a.thumbnail").click(projectClick);
+	$("#togglejs").click(buttonClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'>\
+		<p>Description of the project.</p></div>");
+	} else {
+		description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
+}
+
+function buttonClick(e) {
+	e.preventDefault();
+	$(".jumbotron p").toggleClass("active");
 }
